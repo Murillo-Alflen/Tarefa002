f946ce3f490b96f18946ab1eac7b249f5453c2ef
Murillo-Alflen <murilloalflen@outlook.com>
2024-08-30 21:41:46 -0300

    Update Tarefa.conv

--- Tarefa.c
+++ Tarefa.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
+    char nomeProduto[50];
+    int quantidade;
+    float precoUnitario, valorTotal, valorTotalDia = 0;
+    char opcao;
 
-    float raio, area;
+    do {
+        // Receber dados do usuário
+        printf("Digite o nome do produto: ");
+        scanf("%s", nomeProduto);
+        printf("Digite a quantidade vendida: ");
+        scanf("%d", &quantidade);
+        printf("Digite o preco unitario: ");
+        scanf("%f", &precoUnitario);
 
-    printf("Digite o raio do círculo:\n");
-    scanf("%f", &raio);
+        // Calcular valor total da venda
+        valorTotal = quantidade * precoUnitario;
+        printf("Valor total da venda: %.2f\n", valorTotal);
 
-    area = pi * pow(raio,2);
+        // Adicionar o valor da venda ao total do dia
+        valorTotalDia += valorTotal;
 
-    printf("Área do círculo: %.2f\n", area);
+        // Perguntar se o usuário deseja registrar outra venda
+        printf("Deseja registrar outra venda? (s/n): ");
+        scanf(" %c", &opcao);
+
+    } while(opcao == 's' || opcao == 'S');
+
+    // Exibir o total arrecadado no dia
+    printf("Valor total arrecadado no dia: %.2f\n", valorTotalDia);
 
     return 0;
 }
